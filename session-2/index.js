7d04ff05a4b31ed1f13c7d6b46e3911ed4fe5832
jesvica <jvictoriaw@hotmail.co.uk>
2021-12-06 23:07:41 +0000

    Changes made

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,10 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log('hello world')
+console.log('hello world again')
+console.log('hello jessica')
+console.log(document.querySelector('#description').innerHTML)
+console.log(document.querySelectorAll('.about').innerHTML)
+console.log(document.querySelectorAll('about').innerHTML)
+console.log(document.querySelectorAll('.about'))
+
--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,21 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+function sayHey() {
+    console.log("hey");
+}
+
+function conversation() {
+    sayHey();
+    console.log("How are you?");
+}
+
+sayHey();
+conversation();
+
+function futureAge (name, age) {
+    const ageInFiveYears = age + 5;
+    return "Hi " + name + "! You will be " + age + " in 5 years."
+}
+
+console.log(futureAge(Bob, 21);
+futureAge();
