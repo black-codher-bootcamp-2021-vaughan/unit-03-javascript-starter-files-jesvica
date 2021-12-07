63bc2cc5b1a0830ea7c24a11dd7e44637b3a81b3
jesvica <jvictoriaw@hotmail.co.uk>
2021-12-07 21:41:11 +0000

    Switch statements

--- session-5/index.js
+++ session-5/index.js
@@ -1,21 +1,29 @@
-function getGrade(total) {
-if (total > 90) {
-    console.log("A+"); 
-} else if (total >= 80 && total <= 90) {
-    console.log("A");
-} else if (total >= 70 && total <= 80) {
-    console.log("B");
-} else if (total >= 60 && total <= 70) {
-    console.log("C");
-} else if (total >= 50 && total <= 60) {
-    console.log("D");
-} else if (total >= 40 && total <= 50) {
-    console.log("E");
-} else if (total >= 30 && total <= 40) {
-    console.log("F");
-} else {
-    console.log("Fail");
+function getGrade (total) {
+switch (true) {
+    case total >= 90:
+        return "A+";
+            break;
+    case total >= 80 && total <= 90:
+        return "A";
+            break;
+    case total >= 70 && total <= 80:
+        return "B";
+            break;
+    case total >= 60 && total <= 70:
+        return "C";
+            break;
+    case total >= 50 && total <= 60:
+        return "D";
+            break;
+    case total >= 40 && total <= 50:
+        return "E";
+            break;
+    case total >= 30 && total <= 40:
+        return "F";
+            break;
+    default:
+        return "Fail";
 }
 };
 
-getGrade(40);
+console.log(getGrade(70));
