9f214cad3eeeee8486e5813d81078d67bfeb5729
jesvica <jvictoriaw@hotmail.co.uk>
2022-02-19 14:13:13 +0000

    task 2 - basic maths operators

--- session-4/index.js
+++ session-4/index.js
@@ -34,30 +34,30 @@ function biography(person) {
 console.log(biography(personA));
 console.log(biography(personB));
 console.log(biography(personC));
-console.log(biography(personD));
+console.log(biography(personD))
 
 
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + x + y = " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y = " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * x;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * x = " + multiplication);
 
-// const division = x * y;
+const division = x * y / x;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x + y / x = " + division);
 
 
 
