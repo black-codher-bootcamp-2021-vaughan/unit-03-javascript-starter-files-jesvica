4e0da361dd8a8f797bda12b02549fedd538bf272
jesvica <jvictoriaw@hotmail.co.uk>
2022-02-19 21:11:33 +0000

    task 1 - displayPopulation

--- session-8/js/index.js
+++ session-8/js/index.js
@@ -1,3 +1,10 @@
 import displayPopulation from "./birmingham.js";
 import { fruits } from "./fruit.js";
 import createListOfFruits from "./giveMeFruits.js";
+
+
+const button = document.createElement('button');
+const text = document.createTextNode('Show Population');
+button.appendChild(text);
+button.onclick = displayPopulation;
+document.body.appendChild(button);
