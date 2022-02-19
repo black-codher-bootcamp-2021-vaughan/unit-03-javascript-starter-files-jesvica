85d475a73cb698a926374c67133c816aaf4946b2
jesvica <jvictoriaw@hotmail.co.uk>
2022-02-19 13:59:19 +0000

    task 1 - statements and objects

--- session-4/index.js
+++ session-4/index.js
@@ -1,19 +1,40 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+let personA = {
+    name: 'Jessica',
+    age: '25',
+    location: 'Wolverhampton',
+    likes: 'coding'
+};
 
+let personB = {
+    name: 'Jordan',
+    age: '26',
+    location: 'Wolverhampton',
+    likes: 'Italian food'
+};
 
+let personC = {
+    name: 'Mary',
+    age: '28',
+    location: 'London',
+    likes: 'gardening'
+}
 
+let personD = {
+    name: 'Angus',
+    age: '37',
+    location: 'Edinburgh',
+    likes: 'football'
+};
 
+function biography(person) {
+    return "Hi, my name is " + person.name + ". I am " + person.age + " years old, I live in " + person.location + " and I like " + person.likes + "."
+}
 
-
-
-
-
-
-
-
-
-
-
+console.log(biography(personA));
+console.log(biography(personB));
+console.log(biography(personC));
+console.log(biography(personD));
 
 
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
